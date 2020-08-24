d9a2d3e675f44f3cb724d46ad9e70554deeb6d23
Andrea <dev@andreadev.it>
2020-08-25 00:08:05 +0200

    Completed. To be tested

--- share-button.js
+++ share-button.js
@@ -1,82 +1,215 @@
-(() => {
-    let template = document.createElement("template");
-    template.innerHTML = `
-    <style>
-    #share-text {
-        position: absolute;
-        left: -1000000px;
+let template = document.createElement("template");
+template.innerHTML = `
+<style>
+:host {
+    padding: 0.4em 1em;
+    border: solid 1px #aaa;
+    border-radius: 4px;
+    background-color: #fff;
+    cursor: pointer;
+    user-select: none;
+}
+
+:host(:hover) {
+    background-color: #ddd;
+}
+
+:host(:active) {
+    background-color: #ccc;
+}
+
+#share-text {
+    position: absolute;
+    left: -1000000px;
+}
+</style>
+<slot></slot>
+<input type="text" id="share-text" />
+`
+
+class ShareButton extends HTMLElement {
+
+    get title() {
+        return this.getAttribute("title");
     }
-    </style>
-    <slot></slot>
-    <input type="text" id="share-text" />
-    `
 
-    class ShareButton extends HTMLElement {
-        constructor() {
-            super();
-            this.attachShadow({mode: 'open'});
-        }
+    set title(val) {
+        this.setAttribute("title", val);
+    }
 
-        connectedCallback() {
-            this.shadowRoot.appendChild(template.content.cloneNode(true));
-            
-            this.addEventListener("click", () => {
-                this.triggerShare();
-            }, true);
-        }
+    get text() {
+        return this.getAttribute("text");
+    }
 
-        triggerShare() {
-            /*
-            Share Data = {
-                title: string
-                text: string
-                url: string
-                files: FileList (check for navigator.canShare && navigator.canShare({ fileList }) )
-            } 
-            */
+    set text(val) {
+        this.setAttribute("text", val);
+    }
+
+    get url() {
+        return this.getAttribute("url");
+    }
+
+    set url(val) {
+        this.setAttribute("url", val);
+    }
+
+    get fallbackText() {
+        return this.getAttribute("fallbacktext");
+    }
+
+    set fallbackText(val) {
+        this.setAttribute("fallbacktext", val)
+    }
+
+    constructor() {
+        super();
+        this.attachShadow({mode: 'open'});
+    }
+
+    connectedCallback() {
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        
+        this.addEventListener("click", function (e) {
+            this.triggerShare({
+                title: this.title,
+                text: this.text,
+                url: this.url,
+                fallbackText: this.fallbackText
+            });
+        }, true);
+    }
+
+    triggerShare(data) {
+
+        let event = new CustomEvent("share", {
+            detail: {
+                title: data.title,
+                text: data.text,
+                url: data.url,
+                files: data.files,
+                fallbackText: data.fallbackText,
+            },
+            cancelable: true
+        });
+
+        let continueShare = this.dispatchEvent(event);
+
+        if (continueShare) {
             if (window.navigator.share) {
-                this._share({
-                    title: "Prova web share api",
-                    text: "Questa è solo una prova del web share API",
-                    url: "https://andreadev-it.github.io/share-button-custom-element/"
-                });
+                this._share(data);
             }
             else if (window.navigator.clipboard) {
-                this._clipboardAPI("Con clipboard API");
+                this._clipboardAPI(data.fallbackText || data.text);
             }
             else if (document.execCommand) {
-                this._execCommand("prova");
+                this._execCommand(data.fallbackText || data.text);
             }
         }
+    }
 
-        async _share(data) {
-            try {
-                await navigator.share(data);
-                alert("Condiviso con successo!");
+    async _share(data) {
+        try {
+            await navigator.share(data);
+
+            let event = new CustomEvent("sharesuccess", {
+                detail: {
+                    method: "share-api",
+                    title: data.title,
+                    text: data.text,
+                    url: data.url,
+                    files: data.files,
+                    fallbackText: data.fallbackText
+                }
+            });
+
+            this.dispatchEvent(event);
+        }
+        catch(error) {
+            if (error.name === "AbortError") {
+
+                let event = new CustomEvent("sharecanceled", {
+                    detail: {
+                        method: "share-api",
+                        title: data.title,
+                        text: data.text,
+                        url: data.url,
+                        files: data.files
+                    }
+                });
+
+                this.dispatchEvent(event);
             }
-            catch(error) {
-                if (error.name === "AbortError") alert("Hai cancellato lo share");
-                else alert("Errore!");
-                console.error("Errore durante la condivisione ", error);
+            else {
+                let event = new CustomEvent("sharefailed", {
+                    detail: {
+                        method: "share-api",
+                        title: data.title,
+                        text: data.text,
+                        url: data.url,
+                        files: data.files
+                    }
+                })
+                this.dispatchEvent(event);
             }
         }
+    }
 
-        async _clipboardAPI(text) {
+    async _clipboardAPI(text) {
+        try {
             await navigator.clipboard.writeText(text);
-            alert("testo copiato!");
+
+            let event = new CustomEvent("sharesuccess", {
+                detail: {
+                    method: "clipboard-api",
+                    text: text
+                }
+            });
+
+            this.dispatchEvent(event);
         }
+        catch(error) {
+            let event = new CustomEvent("sharefailed", {
+                detail: {
+                    method: "clipboard-api",
+                    text: text
+                }
+            });
 
-        _execCommand(text) {
-            let textDOM = this.shadowRoot.getElementById("share-text");
-            textDOM.value = text;
+            this.dispatchEvent(event);
 
+            throw "Error while trying to copy to clipboard. Was this event triggered by a user interaction?";
+        }
+    }
+
+    _execCommand(text) {
+        let textDOM = this.shadowRoot.getElementById("share-text");
+        textDOM.value = text;
+
+        try {
             textDOM.select();
             textDOM.setSelectionRange(0,999999);
             document.execCommand("copy");
 
-            alert("Copiato testo");
+            let event = new CustomEvent("sharesuccess", {
+                detail: {
+                    method: "clipboard-legacy",
+                    text: text
+                }
+            });
+            this.dispatchEvent(event);
+        }
+        catch(error) {
+            let event = new CustomEvent("sharefailed", {
+                detail: {
+                    method: "clipboard-legacy",
+                    text: text
+                }
+            });
+            this.dispatchEvent(event);
+
+            throw "Error while trying to copy using 'document.execCommand(`copy`). Check the documentation relative to the browser in use.'";
         }
     }
+}
 
-    window.customElements.define("share-button", ShareButton);
-})();
+window.customElements.define("share-button", ShareButton);
